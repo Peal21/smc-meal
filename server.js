70aee89f2bfdb492509b1788b2cb6500222f1bd5
Peal21 <askpeal@gmail.com>
2024-10-29 16:33:06 +0600

    Fix bcrypt issue

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Required modules
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 const session = require('express-session');
 const path = require('path');
 const ExcelJS = require('exceljs');
